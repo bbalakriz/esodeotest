63f3acf7d2283c4f958d67f34b9f9b91524a1924
bbalakriz <37283315+bbalakriz@users.noreply.github.com>
2023-07-24 16:34:17 +0800

    Update Program.cs

--- esoSampleProject/Program.cs
+++ esoSampleProject/Program.cs
@@ -1,52 +1,27 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
-
-
-////getting configuration 
-
-//IConfiguration configuration= new ConfigurationBuilder()
-//    .AddJsonFile
-
-
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
-
-
-// configuration details
-builder.Configuration.AddJsonFile("appsettings.json",optional: false,reloadOnChange: true);
-builder.Configuration.AddJsonFile("secrets/Secret.json", optional: true, reloadOnChange: true);
-//builder.Configuration.AddUserSecrets<Program>();
-builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
-
-builder.Services.AddMvc(options => options.EnableEndpointRouting = true);
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-    
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.UseRouting();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseEndpoints(endpoints =>
-    {
-        endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
-    });
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
-//var name = app.Configuration["Name"];
-//var name1 = app.Configuration["Logging:LogLevel:Default"];
-
-
-
-
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-app.MapControllers();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
